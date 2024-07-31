0d23a871b85a805a566df3ecc15f81bb16091cb8
bnacato97 <154015095+bnacato97@users.noreply.github.com>
2024-07-31 02:15:51 -0500

    subir cambios de rutas y front1s

--- src/screens/MenuUser.js
+++ src/screens/MenuUser.js
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
-import { screen } from "../utils"; // Asegúrate de que la ruta sea correcta
+import { View, ScrollView, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { FontAwesome } from "@expo/vector-icons"; // Importa FontAwesome desde @expo/vector-icons
+import { FontAwesome } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Button, Text, Surface, useTheme } from "react-native-paper";
+import { screen } from "../utils";
+
 export function MenuUser() {
   const navigation = useNavigation();
   const [iUser, setIUser] = useState(0);
-  const [storedUserId, setStoredUserId] = useState(""); // Estado para el userId
-  const [storedUserType, setStoredUserType] = useState(""); // Estado para el rol del usuario
+  const [storedUserId, setStoredUserId] = useState("");
+  const [storedUserType, setStoredUserType] = useState("");
+
   useEffect(() => {
     const fetchUserId = async () => {
       try {
@@ -27,61 +24,55 @@ export function MenuUser() {
           setIUser(1);
         }
       } catch (error) {
-        console.error(
-          "Error fetching userId from AsyncStorage:",
-          error.message
-        );
-        // Manejo de errores
+        console.error("Error fetching userId from AsyncStorage:", error.message);
       }
     };
 
     fetchUserId();
   }, [navigation]);
 
+  const theme = useTheme();
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() =>
-            navigation.navigate(screen.user.accounts, {
-              type: "edit",
-              userId: storedUserId,
-            })
-          }
-        >
-          <FontAwesome
-            name="list-alt"
-            size={24}
-            color="white"
-            style={styles.icon}
-          />
-          <Text style={styles.buttonText}>Update</Text>
-        </TouchableOpacity>
-        {iUser === 1 ? (
-          <TouchableOpacity
+        <Surface style={styles.surface}>
+          <Button
+            mode="contained"
+            icon={() => <FontAwesome name="list-alt" size={24} color="white" />}
+            onPress={() => navigation.navigate(screen.user.accounts, { type: "edit", userId: storedUserId })}
             style={styles.button}
-            onPress={() => navigation.navigate(screen.user.userlist)}
+            labelStyle={styles.buttonText}
           >
-            <FontAwesome
-              name="list"
-              size={24}
-              color="white"
-              style={styles.icon}
-            />
-            <Text style={styles.buttonText}>Usuarios</Text>
-          </TouchableOpacity>
-        ) : null}
-       
+            Update
+          </Button>
+        </Surface>
+        {iUser === 1 && (
+          <Surface style={styles.surface}>
+            <Button
+              mode="contained"
+              icon={() => <FontAwesome name="list" size={24} color="white" />}
+              onPress={() => navigation.navigate(screen.user.userlist)}
+              style={styles.button}
+              labelStyle={styles.buttonText}
+            >
+              Usuarios
+            </Button>
+          </Surface>
+        )}
       </ScrollView>
     </View>
   );
 }
-//initialParams={{ type: "favo", userId: ""}}
+
 const styles = StyleSheet.create({
   container: {
     paddingTop: 30,
-    backgroundColor: "#f0f0f0",
+  },
+  surface: {
+    marginHorizontal: 10,
+    borderRadius: 30,
+    elevation: 5,
   },
   button: {
     flexDirection: "row",
@@ -89,54 +80,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingVertical: 12,
     paddingHorizontal: 20,
-    marginHorizontal: 10,
-    backgroundColor: "#6200ea",
     borderRadius: 30,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
   buttonText: {
-    color: "#fff",
-    fontSize: 16,
-    marginLeft: 10,
-    fontWeight: "bold",
-  },
-  icon: {
-    marginRight: 10,
-  },
-  sectionsContainer: {
-    paddingHorizontal: 10,
-    paddingTop: 20,
-  },
-  sectionContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    margin: 10,
-    borderRadius: 8,
-    backgroundColor: "#fff",
-    elevation: 5,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-  },
-  sectionImage: {
-    width: 150,
-    height: 100,
-    borderRadius: 8,
-    marginBottom: 10,
-  },
-  sectionTitle: {
     fontSize: 16,
     fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 10,
   },
 });
